1af5c8c6c19e07147123318e5a12372b6f786ca4
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2025-02-09 06:07:00 +0100

    Gráficos y SVGs - Utilidades para Chart.js

--- report-server/src/reports/basic-chart-svr.report.ts
+++ report-server/src/reports/basic-chart-svr.report.ts
@@ -24,9 +24,41 @@ const generateChartImage = async () => {
   return Utils.chartJsToImage(chartConfig);
 };
 
+const generateDonut = async () => {
+  const DATA_COUNT = 5;
+  const NUMBER_CFG = { count: DATA_COUNT, min: 0, max: 100 };
+
+  const data = {
+    labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: Utils.numbers(NUMBER_CFG),
+        backgroundColor: Object.values(Utils.CHART_COLORS),
+      },
+    ],
+  };
+
+  const config = {
+    type: 'doughnut',
+    data: data,
+    options: {
+      title: {
+        display: true,
+        text: 'Chart.js Doughnut Chart',
+      },
+    },
+  };
+
+  return Utils.chartJsToImage(config);
+};
+
 export const getBasicChartSrvReport =
   async (): Promise<TDocumentDefinitions> => {
-    const chart = await generateChartImage();
+    const [chart, chartDonut] = await Promise.all([
+      generateChartImage(),
+      generateDonut(),
+    ]);
 
     return {
       content: [
@@ -40,6 +72,10 @@ export const getBasicChartSrvReport =
         },
         {
           image: chart,
+          width: 500,
+        },
+        {
+          image: chartDonut,
           width: 500,
         },
       ],
